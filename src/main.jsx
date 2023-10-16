6d157fb2f60b8e78db78a9d19351be8fcf41aadc
diogocesar <132657506+diogoascesar@users.noreply.github.com>
2023-10-16 19:36:08 +0100

    setup-commit

---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css';
+import{
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
+
+
+
+const routes = [
+  {
+    path:'/',
+    element: <App/>,
+    errorElement: <div>Error 404 - Not Found</div>,
+    // children:[
+    //   {
+    //     path: '/cart',
+    //     element: <Cart/>
+    //   },
+    //   {
+    //     path: '/',
+    //     element: <Store/>
+    //   }
+    // ]
+  }
+  ]
+  
+  const router = createBrowserRouter(routes)
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <RouterProvider router={router}></RouterProvider>
+    </React.StrictMode>,
+  )
